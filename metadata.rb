84e8f4f414f0a5d787a54542e22576ca74e190a3
Jamie Winsor <jamie@vialstudios.com>
2014-03-28 15:23:24 -0700

    version bump 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "A library cookbook for manipulating archive files"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.1.0"
+version          "0.2.0"
 
 supports "ubuntu"
