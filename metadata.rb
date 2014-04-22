06392168ad6d64dbd04a6b8c40b111d2609b19f2
Jamie Winsor <jamie@vialstudios.com>
2014-04-22 16:54:24 -0700

    version bump 0.4.0

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "A library cookbook for extracting archive files"
 long_description "A library cookbook for extracting archive files"
-version          "0.3.0"
+version          "0.4.0"
 
 supports "ubuntu"
 
-depends "build-essential", "~> 2.0", ">= 2.0.1"
+depends "build-essential", "~> 2.0"
