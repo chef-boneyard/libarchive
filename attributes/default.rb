de8ffb65f17c3aafd5077c788f668c0539714929
GerbenPapo <gerbenpapo@gmail.com>
2015-07-31 11:22:06 +0200

    added libarchive-devel package for ffi support on RHEL platform families

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,6 +6,8 @@
 #
 if platform_family?('debian')
   default['libarchive']['package_name'] = 'libarchive-dev'
+elsif platform_family?('rhel')
+  default['libarchive']['package_name'] = 'libarchive-devel'
 else
   default['libarchive']['package_name'] = 'libarchive'
 end
