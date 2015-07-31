92a85aaa40efb02e949353477ec5c4107e8961eb
Jamie Winsor <jamie@vialstudios.com>
2015-07-31 11:23:23 -0700

    version bump 0.6.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "A library cookbook for extracting archive files"
 long_description "A library cookbook for extracting archive files"
-version          "0.6.0"
+version          "0.6.1"
 
 supports "ubuntu"
 supports "centos"
