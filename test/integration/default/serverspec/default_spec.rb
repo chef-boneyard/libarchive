e243ed49b6febac0f8800ea5f28504202b3e6aa6
Ben Abrams <me@benabrams.it>
2016-05-09 16:35:27 -0700

    removing apt contraint, fixing serverspec test

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
-describe package('libarchive') do
+if os[:family] == 'redhat'
+  package = 'libarchive-devel'
+elsif ['debian', 'ubuntu'].include?(os[:family])
+  package = 'libarchive-dev'
+else
+  package = 'libarchive'
+end
+
+describe package(package) do
   it { should be_installed }
 end
