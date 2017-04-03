bfda2734dece55ae737945a6f965c8d4f690f0ad
Jennifer Davis <iennae@gmail.com>
2017-04-02 23:16:07 -0700

    Remove kind_of from custom resource definition. Modify parameter mode to accept String, and change default configuration to String. Add converge_by blocks Signed-off-by: Jennifer Davis <iennae@gmail.com>

--- resources/file.rb
+++ resources/file.rb
@@ -5,12 +5,12 @@
 # Author:: Jamie Winsor (<jamie@vialstudios.com>)
 #
 
-property :path, kind_of: String, name_property: true, required: true
-property :owner, kind_of: String
-property :group, kind_of: String
-property :mode, kind_of: Integer, default: 0755
-property :extract_to, kind_of: String, required: true
-property :extract_options, kind_of: [Array, Symbol], default: []
+property :path, String, name_property: true, required: true
+property :owner, String
+property :group, String
+property :mode, [String, Integer], default: '0755'
+property :extract_to, String, required: true
+property :extract_options, [Array, Symbol], default: []
 
 require 'fileutils'
 
@@ -18,14 +18,20 @@ action :extract do
   unless ::File.exist?(new_resource.path)
     raise Errno::ENOENT, "no archive found at #{new_resource.path}"
   end
-
-  FileUtils.mkdir_p(new_resource.extract_to, mode: new_resource.mode)
+  
+  unless Dir.exist?(new_resource.extract_to)
+    converge_by("FileUtils.mkdir_p(#{new_resource.extract_to}, mode: #{new_resource.mode})") do
+      FileUtils.mkdir_p(new_resource.extract_to, mode: new_resource.mode.to_i)
+    end
+  end
 
   Chef::Log.info "libarchive_file[#{new_resource.name}] extracting #{new_resource.path} to #{new_resource.extract_to}"
   LibArchiveCookbook::Helper.extract(new_resource.path, new_resource.extract_to,
     Array(new_resource.extract_options))
 
-  if new_resource.owner || new_resource.group
-    FileUtils.chown_R(new_resource.owner, new_resource.group, new_resource.extract_to)
+  if (new_resource.owner || new_resource.group)
+  	converge_by("FileUtils.chown_R(#{new_resource.owner}, #{new_resource.group}, #{new_resource.extract_to})") do
+      FileUtils.chown_R(new_resource.owner, new_resource.group, new_resource.extract_to)
+    end
   end
 end
