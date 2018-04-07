e93e4811dbcffc0661a28fdcee01a9a8d57179e2
Tim Smith <tsmith@chef.io>
2018-04-07 08:30:09 -0700

    Improve logging when the resource converges
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/file.rb
+++ resources/file.rb
@@ -5,32 +5,33 @@
 # Author:: Jamie Winsor (<jamie@vialstudios.com>)
 #
 
-property :path, String, name_property: true, required: true
+property :path, String, name_property: true
 property :owner, String
 property :group, String
-property :mode, [String, Integer], default: '0755'
+property :mode, [String, Integer], default: '755'
 property :extract_to, String, required: true
-property :extract_options, [Array, Symbol], default: []
+property :extract_options, [Array, Symbol], default: lazy { [] }
 
 require 'fileutils'
 
 action :extract do
   unless ::File.exist?(new_resource.path)
-    raise Errno::ENOENT, "no archive found at #{new_resource.path}"
+    raise Errno::ENOENT, "No archive found at #{new_resource.path}!"
   end
 
   unless Dir.exist?(new_resource.extract_to)
-    converge_by("FileUtils.mkdir_p(#{new_resource.extract_to}, mode: #{new_resource.mode})") do
+    converge_by("create directory #{new_resource.extract_to}") do
       FileUtils.mkdir_p(new_resource.extract_to, mode: new_resource.mode.to_i)
     end
   end
 
-  Chef::Log.info "libarchive_file[#{new_resource.name}] extracting #{new_resource.path} to #{new_resource.extract_to}"
-  LibArchiveCookbook::Helper.extract(new_resource.path, new_resource.extract_to,
-    Array(new_resource.extract_options))
+  converge_by("extract #{new_resource.path} to #{new_resource.extract_to}") do
+    LibArchiveCookbook::Helper.extract(new_resource.path, new_resource.extract_to,
+      Array(new_resource.extract_options))
+  end
 
   if new_resource.owner || new_resource.group
-    converge_by("FileUtils.chown_R(#{new_resource.owner}, #{new_resource.group}, #{new_resource.extract_to})") do
+    converge_by("set owner of #{new_resource.extract_to} to #{new_resource.owner}:#{new_resource.group}") do
       FileUtils.chown_R(new_resource.owner, new_resource.group, new_resource.extract_to)
     end
   end
